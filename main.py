45dd2ab09630df0bffe002a28365c6de64f10f12
mkpenta <mpenta@necc.mass.edu>
2023-12-02 08:12:49 -0500

    add a function to parse the round color count into a dictionary

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
+def get_colors_in_round(round_string: str):
+    color_list = round_string.strip().split(",")
+    colors = {"red": 0, "green": 0, "blue": 0}
+    for col in color_list:
+        [count, color] = col.split()
+        colors[color] = int(count)
+    return colors
+
+
+
 def get_impossible_games_list(max_red: int, max_green: int, max_blue: int):
     games_list = []
     with open("input", "r") as file:
         for line in file:
             game_line = line.strip().split(":")[1]
             rounds = game_line.split(";")
-            print(rounds)
+            get_colors_in_round(rounds[0])
 
     return games_list
 
