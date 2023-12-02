d50a4ac7e2c40fc447c30a381fe8b25e0251990c
mkpenta <mpenta@necc.mass.edu>
2023-12-02 08:43:20 -0500

    part 2 - added to get impossible to find min sets (needs refactoring). added function to get powers of sets

--- main.py
+++ main.py
@@ -9,12 +9,14 @@ def get_colors_in_round(round_string: str):
 
 def get_impossible_games_list(max_red: int, max_green: int, max_blue: int):
     games_list = []
+    min_set_for_games = []
     with open("input", "r") as file:
         game_number = 1
         for line in file:
             game_line = line.strip().split(":")[1]
             rounds = game_line.split(";")
             game_is_ok = True
+            min_set = {}
             for game_round in rounds:
                 round_colors = get_colors_in_round(game_round)
                 if round_colors["red"] > max_red:
@@ -23,11 +25,29 @@ def get_impossible_games_list(max_red: int, max_green: int, max_blue: int):
                     game_is_ok = False
                 if round_colors["blue"] > max_blue:
                     game_is_ok = False
+                # min set
+                color_keys = ["red", "green", "blue"]
+                for c in color_keys:
+                    if c not in min_set or round_colors[c] > min_set[c]:
+                        min_set[c] = round_colors[c]
+            min_set_for_games.append(min_set)
             if game_is_ok:
                 games_list.append(game_number)
             game_number += 1
-    return games_list
+    return games_list, min_set_for_games
+
+
+def get_power_of_sets(min_sets: list):
+    powers = []
+    for ms in min_sets:
+        product = 1
+        for color in ms:
+            product *= ms[color]
+        powers.append(product)
+    return powers
 
 
 if __name__ == '__main__':
-    print(sum(get_impossible_games_list(12, 13, 14)))
+    imp, min_set = get_impossible_games_list(12, 13, 14)
+    print(sum(imp))
+    print(get_power_of_sets(min_set))
