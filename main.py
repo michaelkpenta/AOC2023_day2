c859b00248befd51e32fc026a2ca9a836e6995fa
mkpenta <mpenta@necc.mass.edu>
2023-12-02 08:22:35 -0500

    Part 1 complete

--- main.py
+++ main.py
@@ -7,17 +7,27 @@ def get_colors_in_round(round_string: str):
     return colors
 
 
-
 def get_impossible_games_list(max_red: int, max_green: int, max_blue: int):
     games_list = []
     with open("input", "r") as file:
+        game_number = 1
         for line in file:
             game_line = line.strip().split(":")[1]
             rounds = game_line.split(";")
-            get_colors_in_round(rounds[0])
-
+            game_is_ok = True
+            for game_round in rounds:
+                round_colors = get_colors_in_round(game_round)
+                if round_colors["red"] > max_red:
+                    game_is_ok = False
+                if round_colors["green"] > max_green:
+                    game_is_ok = False
+                if round_colors["blue"] > max_blue:
+                    game_is_ok = False
+            if game_is_ok:
+                games_list.append(game_number)
+            game_number += 1
     return games_list
 
 
 if __name__ == '__main__':
-    get_impossible_games_list(20, 20, 20)
+    print(sum(get_impossible_games_list(12, 13, 14)))
